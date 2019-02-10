6066abda236114daa2a9f35249cc2716c7b87ba4
t.moechel <t.moechel@softcreation.de>
2019-02-10 08:49:05 +0100

    test commit

---
+++ src/Description/Description.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+
+const Description = (props) => {
+    <h3> props.Description</h3>
+}
+export default Description
---
+++ src/Title/Title.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+
+const Title = (props) => {
+    <h2> props.Title</h2>
+}
+export default Title
